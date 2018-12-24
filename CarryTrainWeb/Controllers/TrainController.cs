e2599d9ff4e41e9eae198865c1c25f53eb47fe00
严浩淼 <527758393@qq.com>
2018-12-24 18:50:59 +0800

    添加页面处理

---
+++ CarryTrainWeb/Controllers/TrainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarryTrainWeb.Controllers
+{
+    public class TrainController : Controller
+    {
+        // GET: Train
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
